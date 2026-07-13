302f04ad73572e8595a78548644c4c597d717d68
Muhammad Haris <mharis55516@gmail.com>
2026-07-13 11:53:46 +0500

    build: project initialization and dependency setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="CodeAlpha_NetworkSniffer",
+    version="1.0.0",
+    description="A professional network packet sniffer built for CodeAlpha",
+    author="Haris",
+    packages=find_packages(),
+    install_requires=[
+        "scapy>=2.5.0",
+        "rich>=13.0.0",
+        "colorama>=0.4.6"
+    ],
+    entry_points={
+        "console_scripts": [
+            "packet-sniffer=main:main"
+        ]
+    },
+    python_requires=">=3.9",
+)
